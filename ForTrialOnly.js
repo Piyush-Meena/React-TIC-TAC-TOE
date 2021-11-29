34efb068d0ffd6adfb85372dccb610afe812f942
Piyush <94220242+Piyush-Meena@users.noreply.github.com>
2021-11-29 15:06:37 +0530

    ForTrialOnly.js

---
+++ ForTrialOnly.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './my-sass.scss';
+
+const Header = () => {
+  return (
+    <>
+      <h1>Hello Style!</h1>
+      <p>Add a little style!.</p>
+    </>
+  );
+}
+
+ReactDOM.render(<Header />, document.getElementById('root'));
